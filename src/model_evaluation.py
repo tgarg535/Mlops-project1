a4868f9db81e642e36bf7da960ea3d42211c8bab
tgarg535 <tanushgarg26jul@gmail.com>
2025-07-18 03:46:14 +0530

    automated pipeline using dvc and adding dvc live in evaluation metrics

--- src/model_evaluation.py
+++ src/model_evaluation.py
@@ -3,16 +3,17 @@ import numpy as np
 import pandas as pd
 import pickle
 import json
-from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score, precision_score, recall_score, roc_auc_score
 import logging
 import yaml
+from dvclive import Live
 
 # Ensure the "logs" directory exists
 log_dir = 'logs'
 os.makedirs(log_dir, exist_ok=True)
 
 # logging configuration
-logger = logging.getLogger(name='model_evaluation')
+logger = logging.getLogger('model_evaluation')
 logger.setLevel('DEBUG')
 
 console_handler = logging.StreamHandler()
@@ -77,9 +78,19 @@ def evaluate_model(clf, X_test: np.ndarray, y_test: np.ndarray) -> dict:
     """Evaluate the model and return the evaluation metrics."""
     try:
         y_pred = clf.predict(X_test)
+        y_pred_proba = clf.predict_proba(X_test)[:, 1]
 
-        metrics_dict = classification_report(y_test,y_pred,output_dict=True)
-        
+        accuracy = accuracy_score(y_test, y_pred)
+        precision = precision_score(y_test, y_pred)
+        recall = recall_score(y_test, y_pred)
+        auc = roc_auc_score(y_test, y_pred_proba)
+
+        metrics_dict = {
+            'accuracy': accuracy,
+            'precision': precision,
+            'recall': recall,
+            'auc': auc
+        }
         logger.debug('Model evaluation metrics calculated')
         return metrics_dict
     except Exception as e:
@@ -99,8 +110,6 @@ def save_metrics(metrics: dict, file_path: str) -> None:
         logger.error('Error occurred while saving the metrics: %s', e)
         raise
 
-
-
 def main():
     try:
         params = load_params(params_path='params.yaml')
@@ -111,14 +120,19 @@ def main():
         y_test = test_data.iloc[:, -1].values
 
         metrics = evaluate_model(clf, X_test, y_test)
-        save_metrics(metrics,'reports/metrics.json')
 
+        # Experiment tracking using dvclive
+        with Live(save_dvc_exp=True) as live:
+            live.log_metric('accuracy', accuracy_score(y_test, y_test))
+            live.log_metric('precision', precision_score(y_test, y_test))
+            live.log_metric('recall', recall_score(y_test, y_test))
 
+            live.log_params(params)
+        
+        save_metrics(metrics, 'reports/metrics.json')
     except Exception as e:
         logger.error('Failed to complete the model evaluation process: %s', e)
         print(f"Error: {e}")
-    
-
 
 if __name__ == '__main__':
     main()
